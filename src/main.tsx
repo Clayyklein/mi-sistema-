a0bbad9b1dfa7f56a3c5e4528eeaedf4bc44104d
ClayyKlein <ivanrolon473@gmail.com>
2026-03-09 18:11:34 -0300

    fix timeout error

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,33 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AppStateProvider } from "./state/AppState";
 import "./styles/global.css";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
+function registerServiceWorkerLater() {
+  if (!("serviceWorker" in navigator)) return;
+
+  const register = () => {
     navigator.serviceWorker.register("/sw.js").catch((error) => {
       console.error("Error registrando service worker:", error);
     });
-  });
+  };
+
+  if ("requestIdleCallback" in window) {
+    (window as Window & {
+      requestIdleCallback: (callback: () => void) => void;
+    }).requestIdleCallback(register);
+  } else {
+    setTimeout(register, 1500);
+  }
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AppStateProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AppStateProvider>
-  </React.StrictMode>
+  <AppStateProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AppStateProvider>
 );
+
+registerServiceWorkerLater();
